fecf9a80708eb8584fc9bdf5ce6612ffb8d95305
Ahmed Hussein <ahmedhussein@Ahmeds-MacBook-Pro.local>
2022-03-17 10:46:16 -0600

    for loop

--- src/main/java/com/mizzo/TheArrays.java
+++ src/main/java/com/mizzo/TheArrays.java
@@ -11,5 +11,14 @@ public class TheArrays {
         System.out.println(colors[1]);
         System.out.println(colors[0]);
         System.out.println(colors[2]);
+
+        int[] numbers = {100, 200};
+        for( int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+        for( int i = colors.length-1; i >=0; i--){
+            System.out.println(colors[i]);
+        }
     }
 }
