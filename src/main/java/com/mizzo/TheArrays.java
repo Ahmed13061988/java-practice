4709544a5646b88d4e1cd621b63cf1a11741501c
Ahmed Hussein <ahmedhussein@Ahmeds-MacBook-Pro.local>
2022-03-17 10:31:39 -0600

    adding all files

---
+++ src/main/java/com/mizzo/TheArrays.java
@@ -0,0 +1,15 @@
+package com.mizzo;
+
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "Purple";
+        colors[1] = "Blue";
+        System.out.println(Arrays.toString(colors));
+        System.out.println(colors[1]);
+        System.out.println(colors[0]);
+        System.out.println(colors[2]);
+    }
+}
