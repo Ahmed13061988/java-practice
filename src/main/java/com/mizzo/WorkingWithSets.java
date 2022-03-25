983f9d0b737cc37f9badebec102700de4216c5db
Ahmed Hussein <ahmedhussein@Ahmeds-MacBook-Pro.local>
2022-03-25 11:34:49 -0600

    adding class with override hashcode

--- src/main/java/com/mizzo/WorkingWithSets.java
+++ src/main/java/com/mizzo/WorkingWithSets.java
@@ -2,6 +2,7 @@ package com.mizzo;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class WorkingWithSets {
@@ -18,8 +19,37 @@ public class WorkingWithSets {
         balls.forEach(System.out::println);
         balls.remove(new Ball("Red"));
         System.out.println(balls);
+        System.out.println(balls.size());
 
     }
 
-    record Ball(String color){}
+    //record Ball(String color){}
+    public static class Ball {
+        String color;
+
+        public Ball(String color) {
+            this.color = color;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Ball ball = (Ball) o;
+            return Objects.equals(color, ball.color);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(color);
+        }
+
+        @Override
+        public String toString() {
+            return "Ball{" +
+                    "color='" + color + '\'' +
+                    '}';
+        }
+    }
+
 }
