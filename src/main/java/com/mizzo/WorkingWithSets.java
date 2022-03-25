b83100717ccbf55e3464074ed28cc379837a4e11
Ahmed Hussein <ahmedhussein@Ahmeds-MacBook-Pro.local>
2022-03-25 11:05:00 -0600

    adding balls to HashSet

--- src/main/java/com/mizzo/WorkingWithSets.java
+++ src/main/java/com/mizzo/WorkingWithSets.java
@@ -8,7 +8,12 @@ public class WorkingWithSets {
     public static void main(String[] args) {
         Set<Ball> balls = new HashSet<>();
         balls.add(new Ball("Red"));
+        balls.add(new Ball("Yellow"));
+        balls.add(new Ball("Blue"));
+        balls.add(new Ball("Red"));
+        System.out.println(balls);
         //Set will randomly put the elements, there is no order for elements
+        //There are no duplicates allowed in Set
 
     }
 
